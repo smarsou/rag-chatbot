f251d0bc789b11d2c709603592e289281731369a
Zintoulou <34sousou34@gmail.com>
2025-06-01 12:58:10 +0200

    Add a quick script to test the RAG system

---
+++ test_rag.py
@@ -0,0 +1,24 @@
+from openai import OpenAI
+import logging
+import os
+from langchain_openai import OpenAIEmbeddings
+from langchain_postgres import PGVector
+
+embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+connection = input("Paste the url for connection with the Postgres Vector database : ")
+collection_name = "portfolio"
+print(f"Trying connection using connection: {connection} and collection name: {collection_name}")
+vector_store = PGVector(
+    embeddings=embeddings,
+    collection_name=collection_name,
+    connection=connection,
+    use_jsonb=True,
+)
+print("Connection established successfully.")
+
+while True:
+    query = input("Enter your query : ")
+    num_docs = input("How many documents do you want to retrieve ? (default 4) : ")
+    retrieved_docs = vector_store.similarity_search(query, k=num_docs)
+    for doc, i in enumerate(retrieved_docs):
+        print(i + " : " + doc.page_content)
